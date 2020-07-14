2dab8fd5038271a7aab0611bcd9e931fba26c6c2
hugAOA <huguangchaoren@163.com>
2020-07-14 11:15:01 +0800

    my test v2

---
+++ 2.day2/attribute.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: attribute.cpp
+	> Author: huguang
+	> Mail: hug@haizeix.com
+	> Created Time: 二  7/14 10:52:12 2020
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+
+__attribute__((constructor))
+void main2() {
+    printf("hello world\n");
+}
+
+int main() {
+    printf("hello WHU\n");
+    return 0; 
+}
--- 2.day2/include/mytest.h
+++ 2.day2/include/mytest.h
@@ -21,11 +21,33 @@
 #define EXPECT_GE(a, b) EXPECT(a, >=, b)
 #define EXPECT_LE(a, b) EXPECT(a, <=, b)
 
-#define TEST(a, b) void a##_##b()
+#define TEST(a, b) \
+void a##_##b(); \
+__attribute__((constructor)) \
+void add_##a##_##b() { \
+    add_func(a##_##b, #a "." #b); \
+} \
+void a##_##b()
+
+struct Func_Data {
+    void (*func)();
+    const char *func_name;
+} func_arr[100];
+int func_cnt;
+
+void add_func(void (*func)(), const char *func_name) {
+    func_arr[func_cnt].func = func;
+    func_arr[func_cnt].func_name = func_name;
+    func_cnt += 1;
+    return ;
+}
 
 int RUN_ALL_TESTS() {
-    // to do something
-    // run all test case
+    for (int i = 0; i < func_cnt; i++) {
+        printf("run %s\n", func_arr[i].func_name);
+        func_arr[i].func();
+        printf("ok\n");
+    }
     return 0;
 }
 
