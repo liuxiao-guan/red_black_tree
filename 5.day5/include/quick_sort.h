7452361ca079b3287f0484c867251d3a4bc1e1e0
hugAOA <huguangchaoren@163.com>
2020-07-17 10:09:40 +0800

    quick sort v1+v2

---
+++ 5.day5/include/quick_sort.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: quick_sort.h
+	> Author: huguang
+	> Mail: hug@haizeix.com
+	> Created Time: 五  7/17 09:26:45 2020
+ ************************************************************************/
+
+#ifndef _QUICK_SORT_H
+#define _QUICK_SORT_H
+
+void quick_sort_v1(int *arr, int l, int r);
+void quick_sort_v2(int *arr, int l, int r);
+
+
+#endif
